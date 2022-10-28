e21144439e9a85c20083e1ca9fca744d58005e92
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-10-28 10:59:39 +0200

    fix(bazarliste-dynamic): force groups

--- actions/__BazarListeAction.php
+++ actions/__BazarListeAction.php
@@ -46,22 +46,27 @@ class __BazarListeAction extends YesWikiAction
                 function (bool $isDynamic, bool $isAdmin, array $_arg) use ($selectmembers, $selectmembersdisplayfilters) {
                     $replaceTemplate = !$isDynamic && !empty($selectmembers) && !$isAdmin;
                     $options = ['selectmembers' => $selectmembers];
-                    if (!$isDynamic && $selectmembersdisplayfilters) {
+                    if ($selectmembersdisplayfilters) {
                         $groups = $this->formatArray($_GET['groups'] ?? $_arg['groups'] ?? null);
-                        $groupicons = $this->formatArray($_arg['groupicons'] ?? null);
-                        $titles = $this->formatArray($_GET['titles'] ?? $_arg['titles'] ?? null);
-                        array_unshift($groups, CustomSendMailService::KEY_FOR_PARENTS);
-                        array_unshift($groupicons, "");
-                        array_unshift($titles, _t('CUSTOMSENDMAIL_PARENTS_TITLES'));
-                        if ($selectmembers == "members_and_profiles_in_area") {
+                        if (!$isDynamic){
+                            $groupicons = $this->formatArray($_arg['groupicons'] ?? null);
+                            $titles = $this->formatArray($_GET['titles'] ?? $_arg['titles'] ?? null);
                             array_unshift($groups, CustomSendMailService::KEY_FOR_PARENTS);
                             array_unshift($groupicons, "");
-                            array_unshift($titles, _t('CUSTOMSENDMAIL_AREAS_TITLES'));
+                            array_unshift($titles, _t('CUSTOMSENDMAIL_PARENTS_TITLES'));
+                            if ($selectmembers == "members_and_profiles_in_area") {
+                                array_unshift($groups, CustomSendMailService::KEY_FOR_PARENTS);
+                                array_unshift($groupicons, "");
+                                array_unshift($titles, _t('CUSTOMSENDMAIL_AREAS_TITLES'));
+                            }
+                            $options['groups'] = $groups;
+                            $options['groupicons'] = $groupicons;
+                            $options['titles'] = $titles;
+                            $options['customSendMailService'] = $this->getService(CustomSendMailService::class);
+                        } elseif (empty($groups)) {
+                            // force groups for layout
+                            $options['groups'] = [CustomSendMailService::KEY_FOR_PARENTS];
                         }
-                        $options['groups'] = $groups;
-                        $options['groupicons'] = $groupicons;
-                        $options['titles'] = $titles;
-                        $options['customSendMailService'] = $this->getService(CustomSendMailService::class);
                     }
                     return compact(['replaceTemplate','options']);
                 }
